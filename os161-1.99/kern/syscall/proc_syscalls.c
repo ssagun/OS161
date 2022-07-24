fdb38262de39d6148745ece07b1b863624619687
honk89 <snehal.sagun@gmail.com>
2022-07-24 16:56:29 -0400

    maybe working for execv?

--- os161-1.99/kern/syscall/proc_syscalls.c
+++ os161-1.99/kern/syscall/proc_syscalls.c
@@ -247,10 +247,10 @@ int sys_execv(char *progname, char **argv) {
     char **args = args_alloc(argv);
     argcopy_in(args, argv);
 
-    size_t plen = strelen(progname) + 1;
+    size_t plen = strlen(progname) + 1;
     size_t palloc = plen * sizeof(char);
     char *pname = (char *)kmalloc(palloc);
-    copyinstr((const_userptr_t)progname, pname, palloc. &palloc);
+    copyinstr((const_userptr_t) progname, pname, palloc. &palloc);
 
     /* Open the file. */
     result = vfs_open(pname, O_RDONLY, 0, &v);
